6915e0ff4dd761797cdf363dd04396d431af90c6
Daniel Rose <daniel-rose@gmx.de>
2019-08-29 07:44:27 +0200

    - remove availability periods with qty <= 0 - remove sample availability periods

---
+++ src/FondOfSpryker/Client/ConditionalAvailability/Plugin/Elasticsearch/QueryExpander/NoSamplesConditionalAvailabilityQueryExpanderPlugin.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace FondOfSpryker\Client\ConditionalAvailability\Plugin\Elasticsearch\QueryExpander;
+
+use Elastica\Query;
+use Elastica\Query\BoolQuery;
+use Elastica\Query\Wildcard;
+use Generated\Shared\ConditionalAvailability\Search\PageIndexMap;
+use InvalidArgumentException;
+use Spryker\Client\Kernel\AbstractPlugin;
+use Spryker\Client\Search\Dependency\Plugin\QueryExpanderPluginInterface;
+use Spryker\Client\Search\Dependency\Plugin\QueryInterface;
+
+class NoSamplesConditionalAvailabilityQueryExpanderPlugin extends AbstractPlugin implements QueryExpanderPluginInterface
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @api
+     *
+     * @param \Spryker\Client\Search\Dependency\Plugin\QueryInterface $searchQuery
+     * @param array $requestParameters
+     *
+     * @return \Spryker\Client\Search\Dependency\Plugin\QueryInterface
+     */
+    public function expandQuery(QueryInterface $searchQuery, array $requestParameters = [])
+    {
+        $this->getBoolQuery($searchQuery->getSearchQuery())->addMustNot(
+            new Wildcard(PageIndexMap::SKU, '0-')
+        );
+
+        return $searchQuery;
+    }
+
+    /**
+     * @param \Elastica\Query $query
+     *
+     * @throws \InvalidArgumentException
+     *
+     * @return \Elastica\Query\BoolQuery
+     */
+    protected function getBoolQuery(Query $query): BoolQuery
+    {
+        $boolQuery = $query->getQuery();
+        if (!$boolQuery instanceof BoolQuery) {
+            throw new InvalidArgumentException(sprintf(
+                'Localized query expander available only with %s, got: %s',
+                BoolQuery::class,
+                get_class($boolQuery)
+            ));
+        }
+
+        return $boolQuery;
+    }
+}
